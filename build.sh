8ed2aead0d6711d171984ca70dbdb50ca197a21d
Martin Goellnitz <martin@provocon.eu>
2016-11-08 11:10:48 +0100

    Clean up for the post-divshot era

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# This is just the markdown umbrella project - there's nothing in here except text and pointers.
+CWD=`pwd`
+DIR=`dirname $0`
+cd $DIR
+if [ ! -d build ] ; then
+  mkdir build
+fi
+markdown.pl README.md > .content.html
+cat template/pre.html .content.html template/post.html >build/index.html
+cp template/*.png build
+cp template/*.css build
+cp divshot/* build
+cd build
+zip ../collection.zip *
+cd $CWD
